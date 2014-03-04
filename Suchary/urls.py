41efa35f92c5306f6460958ae578c8a168c74f98
Jakub Chmura <chmura.jakub@gmail.com>
2014-03-04 18:50:40 +0100

    Make more google friendly

--- Suchary/urls.py
+++ Suchary/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 from rest_framework import routers
 from django.contrib import admin
 
@@ -12,14 +13,17 @@ dajaxice_autodiscover()
 router = routers.DefaultRouter()
 router.register(r'/obcy', AllViewSet, 'obcy')
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-    url(r'^/?$', 'obcy.views.all_sites'),
-    url(r'^obcy/(?P<jokeslug>.*)/?$', 'obcy.views.one_joke'),
+    url(r'^$', 'obcy.views.all_sites'),
+    url(r'^obcy/(?P<jokeslug>.+)/$', 'obcy.views.one_joke'),
     url(r'^api', include(router.urls)),
-    url(r'^autorskie/?', 'autorski.views.all_jokes'),
-    url(r'^test/?$', 'obcy.views.test'),
+    url(r'^autorskie/$', 'autorski.views.all_jokes'),
+    url(r'^test/$', 'obcy.views.test'),
+    url(r'^(wykop|codzienny|zacny)/(?P<key>.+)$',
+        RedirectView.as_view(url='http://suchary.jakubchmura.pl/obcy/%(key)s', permanent=True)),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
