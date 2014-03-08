039293fd7221fdbc58cfeaddf4f72a6b17798918
Jakub Chmura <chmura.jakub@gmail.com>
2014-03-08 21:56:33 +0100

    Create Facebook User Admin

---
+++ accounts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from accounts.models import FacebookUser
+
+
+class FacebookUserAdmin(admin.ModelAdmin):
+    list_display = ['uid', 'username', 'user']
+
+admin.site.register(FacebookUser, FacebookUserAdmin)
