038ff8933483667cd9cdd25eeecf71328ac12117
Jakub Chmura <chmura.jakub@gmail.com>
2014-03-20 12:45:01 +0100

    Don't display milliseconds

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,8 @@ from obcy.models import Joke
 
 
 class ObcyJokeSerializer(serializers.ModelSerializer):
+    added = serializers.DateTimeField(source='added', format='%Y-%m-%dT%XZ')
+
     class Meta:
         model = Joke
         fields = ('key', 'votes', 'date', 'added', 'url', 'body')
