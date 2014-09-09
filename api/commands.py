83e3a0bdc847dbb8f6e6606ca8b28520fdefe8fc
Jakub Chmura <jakub@jakubchmura.pl>
2014-09-09 23:55:31 +0200

    Handle unregistered devices.

--- api/commands.py
+++ api/commands.py
@@ -28,7 +28,8 @@ def send(data, collapse_key=None, to=None):
         payload.update({'collapse_key': collapse_key})
     logger.debug('Sending GCM data:\n%s\nto %d devices with collapse key %s', data, len(reg_ids), collapse_key)
     r = requests.post(URL, data=json.dumps(payload), headers=HEADER)
-    logger.debug('GCM response:\n%s', r.text)
+    logger.debug('GCM response (status code=%d):\n%s', r.status_code, r.text)
+    handle_gcm_response(r, reg_ids)
 
 
 def edit_joke(key):
@@ -61,3 +62,19 @@ def send_message(title, body, alias=None):
         'text': body
     }
     send(data, to=alias)
+
+
+def handle_gcm_response(r, reg_ids):
+    if r.status_code != 200:
+        logger.error('GCM send code %d\n%s', r.status_code, r.text)
+        return
+    logger.info('GCM response:\n%s', r.text)
+    response = r.json()
+    if response['failure']:
+        for i, result in enumerate(response['results']):
+            if 'error' in result and result['error'] == 'NotRegistered':
+                reg_id = reg_ids[i]
+                device = Device.objects.get(registration_id=reg_id)
+                device.active = False
+                device.save()
+                logger.warn('Device %s is not registered', device.pk)
