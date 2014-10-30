6ed5fc6a4875e563820af86d0b6f3b3354fc3363
Jakub Chmura <jakub@jakubchmura.pl>
2014-10-30 21:13:25 +0100

    Use the version 2.2 of the facebook API.

--- static/js/facebook.js
+++ static/js/facebook.js
@@ -2,19 +2,17 @@ window.fbAsyncInit = function () {
     FB.init({
         appId: '628784013845093',
         status: false,
-        xfbml: false
+        xfbml: true,
+        version: 'v2.2'
     });
 };
 
 (function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/pl_PL/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/pl_PL/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 $(document).ready(function () {
@@ -43,15 +41,13 @@ function FB_login() {
             var uid = response.authResponse.userID;
             var accessToken = response.authResponse.accessToken;
             var expiresIn = response.authResponse.expiresIn;
-            FB.api("/me", function (response) {
-                send_info(uid, accessToken, expiresIn, response.username);
-            });
+            send_info(uid, accessToken, expiresIn, uid);
         } else if (response.status === 'not_authorized') {
-            FB.login(function (response) {
+            FB.login(function () {
                 return FB_login();
             });
         } else {
-            FB.login(function (response) {
+            FB.login(function () {
                 return FB_login();
             });
         }
