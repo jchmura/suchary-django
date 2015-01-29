abaf222ac0a60a1dca85b3178997f8d2e6b4be46
Jakub Chmura <jakub@jakubchmura.pl>
2015-01-29 22:16:56 +0100

    Integrate with django-reversion

--- obcy/admin.py
+++ obcy/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+import reversion
 
 from obcy.models import Joke
 
 
-class JokeAdmin(admin.ModelAdmin):
+class JokeAdmin(reversion.VersionAdmin):
     list_display = ['site', 'key', 'votes', 'date', 'added', 'duplicate', 'hidden']
     list_filter = ['date', 'hidden']
     search_fields = ['body', 'key']
